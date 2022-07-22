623e9d892edd9372ba8cd945cf9d1f15d89c705d
febo <febo@kent.ac.uk>
2022-07-22 16:25:55 +0100

    Initial bot tax implementation

--- programs/candy-guard/src/guards/bot_tax.rs
+++ programs/candy-guard/src/guards/bot_tax.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+use solana_program::{account_info::AccountInfo, program::invoke, system_instruction};
+
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug)]
 pub struct BotTax {
     pub lamports: u64,
@@ -16,14 +18,36 @@ impl Guard for BotTax {
 }
 
 impl Condition for BotTax {
-    fn evaluate(
+    fn evaluate<'info>(
         &self,
-        _ctx: &Context<Mint>,
+        _ctx: &Context<'_, '_, '_, 'info, Mint<'info>>,
         _candy_guard_data: &CandyGuardData,
         _evaluation_context: &mut EvaluationContext,
     ) -> Result<()> {
-        // the purpuse of this guard is to indicate whether the bot tax is anebled or not
+        // the purpuse of this guard is to indicate whether the bot tax is enbled or not
         // and to store the lamports fee
+        Ok(())
+    }
+}
+
+impl BotTax {
+    pub fn punish_bots<'a>(
+        &self,
+        error: Error,
+        bot_account: AccountInfo<'a>,
+        payment_account: AccountInfo<'a>,
+        system_program: AccountInfo<'a>,
+    ) -> Result<()> {
+        msg!(
+            "{}, Candy Guard Botting is taxed at {:?} lamports",
+            error.to_string(),
+            self.lamports
+        );
+        let final_fee = self.lamports.min(bot_account.lamports());
+        invoke(
+            &system_instruction::transfer(bot_account.key, payment_account.key, final_fee),
+            &[bot_account, payment_account, system_program],
+        )?;
         Ok(())
     }
 }
--- programs/candy-guard/src/instructions/mint.rs
+++ programs/candy-guard/src/instructions/mint.rs
@@ -1,9 +1,12 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::sysvar};
+use anchor_spl::token::Token;
+use candy_machine::{self, CandyMachine};
 
-use crate::guards::{cmp_pubkeys, EvaluationContext};
+use crate::guards::EvaluationContext;
 use crate::state::{CandyGuard, CandyGuardData};
+use crate::utils::cmp_pubkeys;
 
-pub fn mint(ctx: Context<Mint>) -> Result<()> {
+pub fn mint<'info>(ctx: Context<'_, '_, '_, 'info, Mint<'info>>, creator_bump: u8) -> Result<()> {
     let info = ctx.accounts.candy_guard.to_account_info();
     let mut data = info.data.borrow_mut();
 
@@ -22,9 +25,15 @@ pub fn mint(ctx: Context<Mint>) -> Result<()> {
 
     for condition in conditions {
         if let Err(error) = condition.evaluate(&ctx, &candy_guard_data, &mut evaluation_context) {
-            return if let Some(_bot_tax) = candy_guard_data.bot_tax {
+            return if let Some(bot_tax) = candy_guard_data.bot_tax {
                 // apply bot tax using bot_tax.lamports as fee
-                msg!("bot_tax applied");
+                bot_tax.punish_bots(
+                    error,
+                    ctx.accounts.payer.to_account_info(),
+                    ctx.accounts.candy_machine.to_account_info(),
+                    ctx.accounts.system_program.to_account_info(),
+                )?;
+
                 Ok(())
             } else {
                 Err(error)
@@ -32,15 +41,114 @@ pub fn mint(ctx: Context<Mint>) -> Result<()> {
         }
     }
 
-    // all guards are successful, forward the transaction to Candy Machine using the
+    // TODO: all guards are successful, forward the transaction to Candy Machine using the
     // price from the evaluation_context
+    msg!(
+        "EvaluationContext: is_presale={}, is_authority={}, discount_price={}",
+        evaluation_context.is_presale,
+        evaluation_context.is_authority,
+        evaluation_context.discount_price
+    );
+
+    let candy_machine_program = ctx.accounts.candy_machine_program.to_account_info();
+    // candy machine mint instruction accounts
+    let mint_ix = candy_machine::cpi::accounts::Mint {
+        candy_machine: ctx.accounts.candy_machine.to_account_info(),
+        candy_machine_creator: ctx.accounts.candy_machine_creator.to_account_info(),
+        payer: ctx.accounts.payer.to_account_info(),
+        wallet: ctx.accounts.wallet.to_account_info(),
+        metadata: ctx.accounts.metadata.to_account_info(),
+        mint: ctx.accounts.mint.to_account_info(),
+        mint_authority: ctx.accounts.mint_authority.to_account_info(),
+        update_authority: ctx.accounts.update_authority.to_account_info(),
+        master_edition: ctx.accounts.master_edition.to_account_info(),
+        token_metadata_program: ctx.accounts.token_metadata_program.to_account_info(),
+        token_program: ctx.accounts.token_program.to_account_info(),
+        system_program: ctx.accounts.system_program.to_account_info(),
+        rent: ctx.accounts.rent.to_account_info(),
+        recent_slothashes: ctx.accounts.recent_slothashes.to_account_info(),
+        instruction_sysvar_account: ctx.accounts.instruction_sysvar_account.to_account_info(),
+    };
+    // candy machine mint CPI
+    if let Err(error) = candy_machine::cpi::mint(
+        CpiContext::new(candy_machine_program, mint_ix),
+        creator_bump,
+    ) {
+        return if let Some(bot_tax) = candy_guard_data.bot_tax {
+            // apply bot tax using bot_tax.lamports as fee
+            bot_tax.punish_bots(
+                error,
+                ctx.accounts.payer.to_account_info(),
+                ctx.accounts.candy_machine.to_account_info(),
+                ctx.accounts.system_program.to_account_info(),
+            )?;
+
+            Ok(())
+        } else {
+            Err(error)
+        };
+    }
 
     Ok(())
 }
 
 #[derive(Accounts)]
+#[instruction(creator_bump: u8)]
 pub struct Mint<'info> {
     #[account(mut)]
     pub candy_guard: Account<'info, CandyGuard>,
+    /// CHECK: account constraints checked in account trait
+    #[account(address = candy_machine::id())]
+    pub candy_machine_program: AccountInfo<'info>,
+    #[account(mut, has_one = wallet)]
+    pub candy_machine: Box<Account<'info, CandyMachine>>,
+    // seeds and bump are not validated by the candy guard, they will be validated
+    // by the CPI'd candy machine mint call
+    /// CHECK: account constraints checked in account trait
+    #[account(mut)]
+    pub candy_machine_creator: UncheckedAccount<'info>,
+    #[account(mut)]
     pub payer: Signer<'info>,
+    /// CHECK: wallet can be any account and is not written to or read
+    #[account(mut)]
+    pub wallet: UncheckedAccount<'info>,
+    // with the following accounts we aren't using anchor macros because they are CPI'd
+    // through to token-metadata which will do all the validations we need on them.
+    /// CHECK: account checked in CPI
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+    /// CHECK: account checked in CPI
+    #[account(mut)]
+    pub mint: UncheckedAccount<'info>,
+    pub mint_authority: Signer<'info>,
+    pub update_authority: Signer<'info>,
+    /// CHECK: account checked in CPI
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+    /// CHECK: account checked in CPI
+    #[account(address = mpl_token_metadata::id())]
+    pub token_metadata_program: UncheckedAccount<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+    /// CHECK: account constraints checked in account trait
+    #[account(address = sysvar::slot_hashes::id())]
+    pub recent_slothashes: UncheckedAccount<'info>,
+    /// CHECK: account constraints checked in account trait
+    #[account(address = sysvar::instructions::id())]
+    pub instruction_sysvar_account: UncheckedAccount<'info>,
+    // remaining accounts:
+    // > only needed if candy machine has a gatekeeper
+    // gateway_token
+    // > only needed if candy machine has a gatekeeper and it has expire_on_use set to true:
+    // gateway program
+    // network_expire_feature
+    // > only needed if candy machine has whitelist_mint_settings
+    // whitelist_token_account
+    // > only needed if candy machine has whitelist_mint_settings and mode is BurnEveryTime
+    // whitelist_token_mint
+    // whitelist_burn_authority
+    // > only needed if candy machine has token mint
+    // token_account_info
+    // transfer_authority_info
 }
