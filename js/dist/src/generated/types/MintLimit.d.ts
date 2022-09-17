915d0984bf44ee47c00a881eef115600a5079411
febo <febo@kent.ac.uk>
2022-09-17 23:46:20 +0100

    Remove dist directory

--- js/dist/src/generated/types/MintLimit.d.ts
+++
@@ -1,6 +0,0 @@
-import * as beet from '@metaplex-foundation/beet';
-export declare type MintLimit = {
-    id: number;
-    limit: number;
-};
-export declare const mintLimitBeet: beet.BeetArgsStruct<MintLimit>;
