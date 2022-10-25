a6642ad0c00d98f39624ed7e0b9d5822f86a5284
febo <febo@metaplex.com>
2022-10-25 01:08:13 +0100

    Add new error messages at the end

--- program/src/errors.rs
+++ program/src/errors.rs
@@ -28,43 +28,30 @@ pub enum CandyGuardError {
     CandyMachineEmpty,
     #[msg("No instruction was found")]
     InstructionNotFound,
-    #[msg("Duplicated group label")]
-    DuplicatedGroupLabel,
-    // collection
     #[msg("Collection public key mismatch")]
     CollectionKeyMismatch,
     #[msg("Missing collection accounts")]
     MissingCollectionAccounts,
     #[msg("Collection update authority public key mismatch")]
     CollectionUpdateAuthorityKeyMismatch,
-    // bot tax
     #[msg("Mint must be the last instructions of the transaction")]
     MintNotLastTransaction,
-    // live date
     #[msg("Mint is not live")]
     MintNotLive,
-    // native price
     #[msg("Not enough SOL to pay for the mint")]
     NotEnoughSOL,
-    // token burn
     #[msg("Token burn failed")]
     TokenBurnFailed,
-    // token burn/gate/payment
     #[msg("Not enough tokens on the account")]
     NotEnoughTokens,
-    // token payment
     #[msg("Token transfer failed")]
     TokenTransferFailed,
-    // third-party signer
     #[msg("A signature was required but not found")]
     MissingRequiredSignature,
-    // gatekeeper
     #[msg("Gateway token is not valid")]
     GatewayTokenInvalid,
-    // end date
     #[msg("Current time is after the set end date")]
     AfterEndDate,
-    // allow list
     #[msg("Current time is not within the allowed mint time")]
     InvalidMintTime,
     #[msg("Address not found on the allowed list")]
@@ -73,24 +60,16 @@ pub enum CandyGuardError {
     MissingAllowedListProof,
     #[msg("Allow list guard is not enabled")]
     AllowedListNotEnabled,
-    // mint limit
     #[msg("The maximum number of allowed mints was reached")]
     AllowedMintLimitReached,
-    #[msg("Duplicated mint limit id")]
-    DuplicatedMintLimitId,
-    // nft burn/gate/payment
     #[msg("Invalid NFT collection")]
     InvalidNftCollection,
-    // nft burn/gate/payment
     #[msg("Missing NFT on the account")]
     MissingNft,
-    // redeemed amount
     #[msg("Current redemeed items is at the set maximum amount")]
     MaximumRedeemedAmount,
-    // authority only
     #[msg("Address not authorized")]
     AddressNotAuthorized,
-    // freeze
     #[msg("Missing freeze instruction data")]
     MissingFreezeInstruction,
     #[msg("Freeze guard must be enabled")]
@@ -107,4 +86,8 @@ pub enum CandyGuardError {
     ThawNotEnabled,
     #[msg("Unlock is not enabled (not all NFTs are thawed)")]
     UnlockNotEnabled,
+    #[msg("Duplicated group label")]
+    DuplicatedGroupLabel,
+    #[msg("Duplicated mint limit id")]
+    DuplicatedMintLimitId,
 }
